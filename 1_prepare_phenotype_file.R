49eb156213bc2aa785c2580c07e8c1202fda5555
Jonas B <jonas.bacelis@gmail.com>
2018-02-21 17:07:40 +0100

    added pheno 2 and pheno 4 script

--- 1_prepare_phenotype_file.R
+++ 1_prepare_phenotype_file.R
@@ -73,8 +73,12 @@ hist(tbl$MatBirthYear,breaks=1000,col="grey")  # maternal birth year
 tbl = tbl[which((tbl$menarc>=9)&(tbl$menarc<=17)),]  
 
 
+#####################
+#####################  PHENOTYPE 1
+#####################
 
-## Phenotype 1
+
+
 table(type12=tbl$mx12,type23=tbl$mx23,useNA = "a")
 table(tbl$mxPAR[which((tbl$mx12==0)&(tbl$mx23==0))],useNA = "a")
 
@@ -144,6 +148,168 @@ file_name_A = paste("phe1_auto_nCaCo-",
 write.table(phe_autosomes,file_name_A,row.names = F,col.names = T,sep="\t",quote=F)  # local save
 
 file_name_X = paste("phe1_xchr_nCaCo-",
+                    sum(phe_xchromosome$miscrr==1,na.rm = T),"-",
+                    sum(phe_xchromosome$miscrr==0,na.rm = T),"_",
+                    git_hash,"_",time_stamp,"_JB.txt",sep="")
+write.table(phe_xchromosome,file_name_X,row.names = F,col.names = T,sep="\t",quote=F)  # local save
+
+# note, for SNPTEST a second header must be added manually (by Julius, after adding PCs) !
+
+
+#####################
+#####################  PHENOTYPE 2:  3 or more miscarriages
+#####################
+
+table(type12=tbl$mx12,type23=tbl$mx23,useNA = "a")
+table(tbl$mxPAR[which((tbl$mx12==0)&(tbl$mx23==0))],useNA = "a")
+
+# define cases:
+ix1 = which( (tbl$mx12==0) & (tbl$mx23 %in% c(3,4)) )
+ix2 = which( (tbl$mx12==1) & (tbl$mx23 %in% c(2,3,4)) )
+ix3 = which( (tbl$mx12==2) & (tbl$mx23 %in% c(1,2,3,4)) )
+ix4 = which(  tbl$mx12==3 )
+ix5 = which(  tbl$mx12==4 )
+ix6 = which( (is.na(tbl$mx12)) & (tbl$mx23 %in% c(3,4)) )
+case_ix = unique(c(ix1,ix2,ix3,ix4,ix5,ix6))
+length(case_ix)
+
+# define controls:
+contr_ix = which((tbl$mxPAR>0)&(tbl$mx12==0)&(tbl$mx23==0))
+length(contr_ix)
+
+cases = tbl$SentrixID_1[case_ix]
+contr = tbl$SentrixID_1[contr_ix]
+
+#####
+##### prepare PHENOTYPE files
+#####
+
+##
+##  AUTOSOMES
+##
+
+phe = read.table("~/Biostuff/mount_hunt/GWAS_MISCARRIAGE/momIDs_autosom.txt",h=F,stringsAsFactors = F)
+colnames(phe)[1] = "ID_1"
+phe$ID_2 = phe$ID_1
+phe$missing = 0
+phe$miscrr = NA
+phe$miscrr[which(phe$ID_1 %in% cases)] = 1
+phe$miscrr[which(phe$ID_1 %in% contr)] = 0
+phe$seq = seq(nrow(phe))
+
+mby = tbl[,c("SentrixID_1","MatBirthYear")]
+tmp = merge(phe,mby,by.x="ID_1",by.y="SentrixID_1",all.x=T)
+tmp = tmp[order(tmp$seq),]
+phe_autosomes = tmp[,c("ID_1","ID_2","missing","miscrr","MatBirthYear")]
+
+##
+##  X-chromosome
+##
+
+phe = read.table("~/Biostuff/mount_hunt/GWAS_MISCARRIAGE/momIDs_xchromo.txt",h=F,stringsAsFactors = F)
+colnames(phe)[1] = "ID_1"
+phe$ID_2 = phe$ID_1
+phe$missing = 0
+phe$miscrr = NA
+phe$miscrr[which(phe$ID_1 %in% cases)] = 1
+phe$miscrr[which(phe$ID_1 %in% contr)] = 0
+phe$seq = seq(nrow(phe))
+
+mby = tbl[,c("SentrixID_1","MatBirthYear")]
+tmp = merge(phe,mby,by.x="ID_1",by.y="SentrixID_1",all.x=T)
+tmp = tmp[order(tmp$seq),]
+phe_xchromosome = tmp[,c("ID_1","ID_2","missing","miscrr","MatBirthYear")]
+
+#####
+##### export pheno files
+#####
+
+file_name_A = paste("phe2_auto_nCaCo-",
+                    sum(phe_autosomes$miscrr==1,na.rm = T),"-",
+                    sum(phe_autosomes$miscrr==0,na.rm = T),"_",
+                    git_hash,"_",time_stamp,"_JB.txt",sep="")
+write.table(phe_autosomes,file_name_A,row.names = F,col.names = T,sep="\t",quote=F)  # local save
+
+file_name_X = paste("phe2_xchr_nCaCo-",
+                    sum(phe_xchromosome$miscrr==1,na.rm = T),"-",
+                    sum(phe_xchromosome$miscrr==0,na.rm = T),"_",
+                    git_hash,"_",time_stamp,"_JB.txt",sep="")
+write.table(phe_xchromosome,file_name_X,row.names = F,col.names = T,sep="\t",quote=F)  # local save
+
+# note, for SNPTEST a second header must be added manually (by Julius, after adding PCs) !
+
+
+
+#####################
+#####################  PHENOTYPE 4:  at least 1 miscarriage
+#####################
+
+table(type12=tbl$mx12,type23=tbl$mx23,useNA = "a")
+table(tbl$mxPAR[which((tbl$mx12==0)&(tbl$mx23==0))],useNA = "a")
+
+# define cases:
+ix = which( (tbl$mx12 %in% c(1,2,3,4)) | (tbl$mx23 %in% c(1,2,3,4)) )
+case_ix = unique(ix)
+length(case_ix)
+
+# define controls:
+contr_ix = which((tbl$mxPAR>0)&(tbl$mx12==0)&(tbl$mx23==0))
+length(contr_ix)
+
+cases = tbl$SentrixID_1[case_ix]
+contr = tbl$SentrixID_1[contr_ix]
+
+#####
+##### prepare PHENOTYPE files
+#####
+
+##
+##  AUTOSOMES
+##
+
+phe = read.table("~/Biostuff/mount_hunt/GWAS_MISCARRIAGE/momIDs_autosom.txt",h=F,stringsAsFactors = F)
+colnames(phe)[1] = "ID_1"
+phe$ID_2 = phe$ID_1
+phe$missing = 0
+phe$miscrr = NA
+phe$miscrr[which(phe$ID_1 %in% cases)] = 1
+phe$miscrr[which(phe$ID_1 %in% contr)] = 0
+phe$seq = seq(nrow(phe))
+
+mby = tbl[,c("SentrixID_1","MatBirthYear")]
+tmp = merge(phe,mby,by.x="ID_1",by.y="SentrixID_1",all.x=T)
+tmp = tmp[order(tmp$seq),]
+phe_autosomes = tmp[,c("ID_1","ID_2","missing","miscrr","MatBirthYear")]
+
+##
+##  X-chromosome
+##
+
+phe = read.table("~/Biostuff/mount_hunt/GWAS_MISCARRIAGE/momIDs_xchromo.txt",h=F,stringsAsFactors = F)
+colnames(phe)[1] = "ID_1"
+phe$ID_2 = phe$ID_1
+phe$missing = 0
+phe$miscrr = NA
+phe$miscrr[which(phe$ID_1 %in% cases)] = 1
+phe$miscrr[which(phe$ID_1 %in% contr)] = 0
+phe$seq = seq(nrow(phe))
+
+mby = tbl[,c("SentrixID_1","MatBirthYear")]
+tmp = merge(phe,mby,by.x="ID_1",by.y="SentrixID_1",all.x=T)
+tmp = tmp[order(tmp$seq),]
+phe_xchromosome = tmp[,c("ID_1","ID_2","missing","miscrr","MatBirthYear")]
+
+#####
+##### export pheno files
+#####
+
+file_name_A = paste("phe4_auto_nCaCo-",
+                    sum(phe_autosomes$miscrr==1,na.rm = T),"-",
+                    sum(phe_autosomes$miscrr==0,na.rm = T),"_",
+                    git_hash,"_",time_stamp,"_JB.txt",sep="")
+write.table(phe_autosomes,file_name_A,row.names = F,col.names = T,sep="\t",quote=F)  # local save
+
+file_name_X = paste("phe4_xchr_nCaCo-",
                     sum(phe_xchromosome$miscrr==1,na.rm = T),"-",
                     sum(phe_xchromosome$miscrr==0,na.rm = T),"_",
                     git_hash,"_",time_stamp,"_JB.txt",sep="")
